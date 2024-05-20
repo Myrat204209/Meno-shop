0eb85f7c9aad328340517882ca718a5f1d84f847
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2024-05-20 15:21:40 +0500

    comiit #13 part-3

--- lib/favorites/view/favorites_page.dart
+++ lib/favorites/view/favorites_page.dart
@@ -3,6 +3,7 @@ import 'package:app_ui/app_ui.dart';
 import 'package:flutter/material.dart';
 import 'package:meno_shop/favorites/favorites.dart';
 
+//TODO: Change Favorites page into Filter page
 class FavoritesPage extends StatelessWidget {
   const FavoritesPage({super.key});
 
@@ -11,29 +12,52 @@ class FavoritesPage extends StatelessWidget {
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.all(10.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 30),
-            const FilterPageWrap(
-              label: '',
-              children: [],
-            ),
-            const FilterPageWrap(
-              children: categoryChipList,
-              label: 'Categories',
-            ),
-            const FilterPageWrap(children: sizeChipList, label: 'Size'),
-            const FilterPageWrap(children: brandChipList, label: 'Brands'),
-            const Expanded(child: SizedBox()),
-            AppButton(
-              type: AppButtonType.large,
-              text: 'Close',
-              onTap: () {},
-            )
-          ],
+        child: TextButton(
+          onPressed: () {
+            showModalBottomSheet(
+              context: context,
+              backgroundColor: UIColors.quaterniary,
+              isScrollControlled: true,
+              builder: (context) => const FractionallySizedBox(
+                heightFactor: 0.9,
+                child: FilterModalContent(),
+              ),
+            );
+          },
+          child: const Text('Press this button'),
         ),
       ),
+    );
+  }
+}
+
+class FilterModalContent extends StatelessWidget {
+  const FilterModalContent({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const FilterPageWrap(
+          label: '',
+          children: [],
+        ),
+        const FilterPageWrap(
+          children: categoryChipList,
+          label: 'Categories',
+        ),
+        const FilterPageWrap(children: sizeChipList, label: 'Size'),
+        const FilterPageWrap(children: brandChipList, label: 'Brands'),
+        const Expanded(child: SizedBox()),
+        AppButton(
+          type: AppButtonType.large,
+          text: 'Close',
+          onTap: () {},
+        )
+      ],
     );
   }
 }
