af2aafe31d7c563abb830bcb17adcfb8c97b6815
Myrat204209 <myrat.annamuhammedov2002@gmail.com>
2024-05-23 10:40:00 +0500

    commit #15 - Localizations

---
+++ lib/language/bloc/language_bloc.dart
@@ -0,0 +1,36 @@
+import 'dart:async';
+
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+
+part 'language_event.dart';
+
+const kDefaultLocale = Locale('en');
+
+class LanguageBloc extends HydratedBloc<LanguageEvent, Locale> {
+  LanguageBloc() : super(kDefaultLocale) {
+    on<LanguageChanged>(_onLanguageChanged);
+  }
+
+  FutureOr<void> _onLanguageChanged(
+    LanguageChanged event,
+    Emitter<Locale> emit,
+  ) async {
+    try {
+      emit(event.locale);
+    } catch (error, stackTrace) {
+      addError(error, stackTrace);
+    }
+  }
+
+  @override
+  Locale? fromJson(Map<String, dynamic> json) {
+    final languageCode = json['languageCode'];
+    return languageCode != null ? Locale(languageCode) : kDefaultLocale;
+  }
+
+  @override
+  Map<String, dynamic>? toJson(Locale state) =>
+      {'languageCode': state.languageCode};
+}
